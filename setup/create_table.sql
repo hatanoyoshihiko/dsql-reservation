a51d55023aa6dbf8fc146eeb36d28a76fa9f0c3e
Hatano Yoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2025-07-23 19:44:44 +0900

    Added unique constraint

--- setup/create_table.sql
+++ setup/create_table.sql
@@ -1,5 +1,7 @@
+-- 既存のテーブルがあれば削除
 DROP TABLE IF EXISTS reservations;
 
+-- 新しい予約テーブルを作成（reserved_date に UNIQUE 制約あり）
 CREATE TABLE IF NOT EXISTS reservations (
   id UUID PRIMARY KEY,
   name TEXT NOT NULL,
